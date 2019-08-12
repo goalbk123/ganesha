c7964e2b5d5759b955e7ab5d55e987a111f8f13d
Bharani <bharani.r@skillrary.com>
2019-08-12 18:39:45 +0530

    second

--- src/test/java/sample/Login.java
+++ src/test/java/sample/Login.java
@@ -7,16 +7,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login {
-	public WebDriver driver;
+
 	@Test
 	public void openapplication()
 	{
 //		WebDriverManager.chromedriver().setup();
 //		driver=new ChromeDriver();
 //		driver.get("https://www.amazon.com/");
-		System.out.println(driver.getTitle());
+		System.out.println("Srinidhi");
 		
-		driver.close();
+		
 	}
 
 }
